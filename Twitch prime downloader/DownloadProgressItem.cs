60143d5d57271adadcd47ed187ec0e2ca53cab9a
BlackRain <masaraksh86@mail.ru>
2026-08-17 18:15:46 +0700

    Исправлена табуляция в классе "DownloadProgressItem"

--- Twitch prime downloader/DownloadProgressItem.cs
+++ Twitch prime downloader/DownloadProgressItem.cs
@@ -3,14 +3,14 @@ namespace Twitch_prime_downloader
 {
 	internal class DownloadProgressItem
 	{
-        internal int TaskId { get; }
-        internal TwitchVodChunkDownloader ChunkDownloader { get; }
-        internal long ChunkSize { get; }
-        internal long DownloadedSize { get; }
-        internal int ErrorCode { get; }
-        internal DownloadItemState State { get; }
+		internal int TaskId { get; }
+		internal TwitchVodChunkDownloader ChunkDownloader { get; }
+		internal long ChunkSize { get; }
+		internal long DownloadedSize { get; }
+		internal int ErrorCode { get; }
+		internal DownloadItemState State { get; }
 
-        internal DownloadProgressItem(int taskId, TwitchVodChunkDownloader chunkDownloader,
+		internal DownloadProgressItem(int taskId, TwitchVodChunkDownloader chunkDownloader,
 			long chunkSize, long downloadedSize, int errorCode, DownloadItemState state)
 		{
 			TaskId = taskId;
